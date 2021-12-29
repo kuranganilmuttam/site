877733c56584b69bd222cf71c8ee4685bbcc575d
Senthil <senthil@rippling.com>
2021-12-29 10:55:09 +0530

    home page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,141 +1,89 @@
 import * as React from "react"
 
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 className="text-7xl font-semibold text-yellow-800	">
-        Kuranganilmuttam Valeeswarar Koil
-      </h1>
+    <main>
+      <title>Kuranganilmuttam Valeeswarar Koil</title>
+      <div class="text-center text-yellow-800">
+        <h1 class="text-3xl font-semibold py-10">
+          அருள்மிகு இறையார் வளையம்மை சமேத <br />
+          அருள்மிகு வாலீஸ்வரர் திருக்கோயில்
+        </h1>
+        <h2 class="text-xl">குரங்கணில்முட்டம், தூசி அஞ்சல் - 631702</h2>
+        <h2 class="text-xl">வெம்பாக்கம் வட்டம், திருவண்ணாமலை மாவட்டம்</h2>
 
-      <ul style={listStyles}>
+      </div>
+      <div class="grid grid-cols-6 p-5 text-gray-600">
+        <div class="col-span-1"></div>
+        <div class="text-justify col-span-4">
+          <div class="py-5 grid grid-cols-3">
+            <div></div>
+            <div class="text-center">
+              <div class="italic py-5">
+                தென்னாடுடைய சிவனே போற்றி!<br />
+                எந்நாட்டவர்க்கும் இறைவா போற்றி!
+              </div>
+              <div>
+                மையார் நிற மேனி அரக்கர் தம் கோனை <br />
+                உய்யா வகையால் அடர்த்து இன்னருள் செய்த<br />
+                கொய்யார் மலர் சூடி குரங்கணில்முட்டம்<br />
+                கையால் தொழுவார் வினை காண்டல் அரிதே!<br />
+                -திருஞானசம்பந்தர்
+              </div>
+            </div>
+            <div></div>
+          </div>
+          <div class="px-20">
+            இத்திருத்தலம்,
+            தொண்டை மண்டலத்திலே பாடல் பெற்ற சிவத்தலங்கள் 32-ல் காஞ்சீபுரத்தில் முதல் 5 தலங்களும்,
+            அடுத்து 6-வது திருத்தலமாக விளங்குகிறது. கி.பி.7-ஆம் நூற்றாண்டில் (637-ம் வருடம்) சுமார் 1400
+            ஆண்டுகளுக்கு முன்னர் திருஞானசம்பந்தர் தேவார பதிகம் பாடிய பாடல் பெற்ற திருத்தலம். இந்த
+            ஆலயத்தில் சுவாமி மேற்கு திசையை நோக்கி எழுந்தருளி அருள்பாலிக்கிறார்.
+            அட்டதிக்கிலும் சிவ பூஜை செய்த வாலி குரங்கு வடிவிலும், தேவேந்திரன் அணில் வடிவிலும்,
+            திருக்கடையூரிலே மார்க்கண்டேயரைப் பிடிக்க வந்த எமன், சிவனையும் பிடித்ததினால் சிவனால் வீசி
+            எறியப்பட்டு, இத்தலத்திற்கு வந்து காக்கை வடிவிலே சுவாமியின் முப்புரமும் தனது அலகினால் கீறி
+            தீர்த்தம் கண்டெடுத்து வாலீஸ்வர சுவாமிக்கு அபிஷேக ஆராதனைகள் செய்து விமோசனம் பெற்ற
+            தலமாகும். தேவர்கள் பல்வேறு வடிவங்களில் பூஜித்து விமோசனம் பெற்ற தலம் இத்திருத்தலம்.
+
+            <div class="py-10">
+              <h4 class="font-semibold">ஆலயத்தின் சிறப்பு அம்சங்கள்</h4>
+              குழந்தைகளுக்கு ஏற்படும் பிணிகள் தீர (பாலாரிஷ்டம் விலக), திருமண தடைகள் விலக,
+              சுகப்பிரசவம் நடக்க, பிரிந்த தம்பதியர் ஒன்று சேர, குழந்தை பாக்கியம் பெற இத்தலத்து
+              அருள்மிகு இறையார் வளையம்மை உடனுறை அருள்மிகு வாலீஸ்வர சுவாமியை வழிபாடு செய்ய
+              அனைத்து பயன்களும் சித்தியாகும் என்பது நிதர்சனமான உண்மை.
+            </div>
+            இத்திருத்தல பாலாலய கும்பாபிஷேகம் கடந்த கார்த்திகை மாதம் 27 ஆம் நாள் (13.12.2021) அன்று
+            இனிதே நடந்தேறியது. தொடர்ந்து ஆலய திருப்பணிகள் நடைபெற்று வருகின்றது.
+
+            எனவே, மேற்படி ஆலய திருப்பணிகள் தொடர்ந்து நடைபெறவும், விரைந்து முடிக்கவும் இதனைக்
+            கண்ணுறும் அன்பர்கள், அருளாளர்கள், பக்தகோடிகள் அனைவரும் தங்களால் இயன்ற பொருளுதவி
+            அளிக்கவும், இயன்ற ஏதேனும் ஒரு திருப்பணியினை ஏற்று நடத்தி தந்து ஈசனின் இன்னருள் பெற்று
+            உய்யவும் பணிந்து வேண்டுகிறோம்.
+            <div class="py-10 grid grid-cols-3">
+              <div></div>
+              <div class="font-semibold">
+                A/c No: 311622010000481 <br />
+                Name: K. M NATARAJAN <br />
+                BANK NAME: UNION BANK OF INDIA  BRANCH. DUSI BRANCH <br />
+                IFSC CODE: UBIN0931161 <br />
+                <p class="pt-5">
+                  Other join account holders:
+                  <ul>
+                    <li>E. PUGAZHENDI</li>
+                    <li>K. PANNEERASELVAM</li>
+                    <li>S. ELUMALAI</li>
+                  </ul>
+                </p>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-span-1"></div>
+      </div>
+      {/* <ul style={listStyles}>
         <li style={docLinkStyle}>
           <a
             style={linkStyle}
@@ -162,8 +110,8 @@ const IndexPage = () => {
             </span>
           </li>
         ))}
-      </ul>
-    </main>
+      </ul> */}
+    </main >
   )
 }
 
