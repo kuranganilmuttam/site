4352b57e336666bd146a06ac5b64f5dd2192c465
Senthil <senthil@rippling.com>
2022-01-05 08:17:23 +0530

    change menu highlight

--- src/components/layout.js
+++ src/components/layout.js
@@ -32,18 +32,18 @@ export default function Layout({ children }) {
                             <div class="mx-auto p-4">
                                 <div class="pt-2 pb-2 hidden md:block py-15 border-dotted border-y-2">
                                     <div class="flex justify-center flex-row px-10 text-gray-500 font-semibold">
-                                        <div class="pr-5 hover:text-blue-500 transition duration-300"><a href="/" class={isPathMatching("/") ? 'text-yellow-800' : ''} >முகப்பு/Home</a></div>
-                                        <div class="px-5 hover:text-blue-500 transition duration-300"><a href="/videos" class={isPathMatching("/videos") ? 'text-yellow-800' : ''}>காணொளிகள்/Videos</a></div>
-                                        <div class="px-5 hover:text-blue-500 transition duration-300"><a href="/works" class={isPathMatching("/works") ? 'text-yellow-800' : ''}>திருப்பணி/Renovation </a></div>
-                                        <div class="px-5 hover:text-blue-500 transition duration-300"><a href="/contact" class={isPathMatching("/contact") ? 'text-yellow-800' : ''}>தொடர்பு & வழி/Contact</a></div>
+                                        <div class="pr-5 hover:text-blue-500 transition duration-300"><a href="/" class={isPathMatching("/") ? 'text-yellow-800' : ''} >முகப்பு / Home</a></div>
+                                        <div class="px-5 hover:text-blue-500 transition duration-300"><a href="/videos" class={isPathMatching("/videos") ? 'text-yellow-800' : ''}>காணொளிகள் / Videos</a></div>
+                                        <div class="px-5 hover:text-blue-500 transition duration-300"><a href="/works" class={isPathMatching("/works") ? 'text-yellow-800' : ''}>திருப்பணி / Renovation </a></div>
+                                        <div class="px-5 hover:text-blue-500 transition duration-300"><a href="/contact" class={isPathMatching("/contact") ? 'text-yellow-800' : ''}>தொடர்பு & வழி / Contact</a></div>
                                     </div>
                                 </div>
 
                             </div>
-                            <div class="md:hidden flex justify-between flex-col border border-y-1 my-5 p-3 bg-yellow-800">
+                            <div class="md:hidden flex justify-between flex-col border border-y-1 my-5 px-3 bg-yellow-600">
                                 <button class="outline-none mobile-menu-button px-10 text-white">
                                     <div class="flex flex-row justify-between">
-                                        <div class="px-10 text-center ">மற்ற பக்கங்கள்/Menu</div>
+                                        <div class="px-10 text-center ">பக்கங்கள்/Menu</div>
                                         <div>
                                             <svg class=" w-6 h-6 hover:text-gray-500 "
                                                 x-show="!showMenu"
@@ -58,19 +58,19 @@ export default function Layout({ children }) {
                                         </div>
                                     </div>
                                 </button>
-                                <div class="hidden mobile-menu text-white">
-                                    <div class="flex flex-col text-center px-10" >
+                                <div class="mobile-menu text-white">
+                                    <div class="flex flex-col text-center" >
                                         <div class="border-b pb-2"></div>
-                                        <div class="px-5 py-1 hover:text-blue-500 transition duration-300 border-b"><a href="/" class={isPathMatching("/") ? 'font-bold' : ''} >முகப்பு/Home</a></div>
-                                        <div class="px-5 py-1 hover:text-blue-500 transition duration-300 border-b"><a href="/videos" class={isPathMatching("/videos") ? 'font-bold' : ''}>காணொளிகள்/Videos</a></div>
-                                        <div class="px-5 py-1 hover:text-blue-500 transition duration-300 border-b"><a href="/works" class={isPathMatching("/works") ? 'font-bold' : ''}>திருப்பணி/Renovation </a></div>
-                                        <div class="px-5 pt-1 hover:text-blue-500 transition duration-300"><a href="/contact" class={isPathMatching("/contact") ? 'font-bold' : ''}>தொடர்பு & வழி/Contact</a></div>
+                                        <a href="/" class={isPathMatching("/") ? 'bg-cyan-600' : ''} ><div class="px-5 py-1 border-b">முகப்பு / Home</div></a>
+                                        <a href="/videos" class={isPathMatching("/videos") ? 'bg-cyan-600' : ''}><div class="px-5 py-1 border-b">காணொளிகள் / Videos</div></a>
+                                        <a href="/works" class={isPathMatching("/works") ? 'bg-cyan-600	' : ''}><div class="px-5 py-1 border-b">திருப்பணி / Renovation </div></a>
+                                        <a href="/contact" class={isPathMatching("/contact") ? 'bg-cyan-600' : ''}><div class="px-5 py-1"> தொடர்பு & வழி / Contact</div></a>
                                     </div>
                                 </div>
                             </div>
                         </nav>
                     </div>
-                </div>
+                </div >
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
                     {children}
                 </div>
@@ -82,6 +82,6 @@ export default function Layout({ children }) {
                 </div>
                 &copy;2022 Kuranganilmuttam.org
             </footer>
-        </div>
+        </div >
     )
 }
