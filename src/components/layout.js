dbff923db38d7f816a4cfdaa640d5e63e3452afa
Senthil <senthil@rippling.com>
2022-01-05 07:50:25 +0530

    mobile menu and refactoring nav highlight to work with trailing slashes

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,14 @@ import { useEffect } from "react";
 
 export default function Layout({ children }) {
     useEffect(() => {
-        // const btn = document.querySelector("button.mobile-menu-button");
-        // const menu = document.querySelector(".mobile-menu");
-        // btn.addEventListener("click", function () { menu.classList.toggle("hidden") });
+        const btn = document.querySelector("button.mobile-menu-button");
+        const menu = document.querySelector(".mobile-menu");
+        btn.addEventListener("click", function () { menu.classList.toggle("hidden") });
     }, [])
     const isBrowser = () => typeof window !== "undefined"
+    const isPathMatching = (path) => {
+        return isBrowser() && window.location.pathname.replace(/\/+$/, '') === path.replace(/\/+$/, '')
+    }
     return (
         <div class="flex flex-col text-gray-600 h-screen justify-between">
             <main>
@@ -26,37 +29,45 @@ export default function Layout({ children }) {
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
                     <div class="text-justify col-span-4">
                         <nav>
-                            <div class="max-w-6xl mx-auto p-4">
-                                <div class="pt-2 pb-2 border-dotted border-y-2">
-                                    <div class="flex justify-center flex-row px-10 text-gray-500 font-semibold hover:text-blue-500 transition duration-300">
-                                        <div class="pr-5"><a href="/" class={isBrowser() && window.location.pathname === "/" ? 'text-yellow-800' : ''} >முகப்பு/Home</a></div>
-                                        <div class="px-5"><a href="/videos" class={isBrowser() && window.location.pathname === "/videos" ? 'text-yellow-800' : ''}>காணொளிகள்/Videos</a></div>
-                                        <div class="px-5"><a href="/works" class={isBrowser() && window.location.pathname === "/works" ? 'text-yellow-800' : ''}>திருப்பணி/Renovation </a></div>
-                                        <div class="px-5"><a href="/contact" class={isBrowser() && window.location.pathname === "/contact" ? 'text-yellow-800' : ''}>தொடர்பு & வழி/Contact</a></div>
+                            <div class="mx-auto p-4">
+                                <div class="pt-2 pb-2 hidden md:block py-15 border-dotted border-y-2">
+                                    <div class="flex justify-center flex-row px-10 text-gray-500 font-semibold">
+                                        <div class="pr-5 hover:text-blue-500 transition duration-300"><a href="/" class={isPathMatching("/") ? 'text-yellow-800' : ''} >முகப்பு/Home</a></div>
+                                        <div class="px-5 hover:text-blue-500 transition duration-300"><a href="/videos" class={isPathMatching("/videos") ? 'text-yellow-800' : ''}>காணொளிகள்/Videos</a></div>
+                                        <div class="px-5 hover:text-blue-500 transition duration-300"><a href="/works" class={isPathMatching("/works") ? 'text-yellow-800' : ''}>திருப்பணி/Renovation </a></div>
+                                        <div class="px-5 hover:text-blue-500 transition duration-300"><a href="/contact" class={isPathMatching("/contact") ? 'text-yellow-800' : ''}>தொடர்பு & வழி/Contact</a></div>
                                     </div>
-                                    {/* <div class="md:hidden flex items-center">
-                                        <button class="outline-none mobile-menu-button">
-                                            <svg class=" w-6 h-6 text-gray-500 hover:text-green-500 "
+                                </div>
+
+                            </div>
+                            <div class="md:hidden flex justify-between flex-col border border-y-1 my-5 p-3 bg-yellow-800">
+                                <button class="outline-none mobile-menu-button px-10 text-white">
+                                    <div class="flex flex-row justify-between">
+                                        <div class="px-10 text-center ">மற்ற பக்கங்கள்/Menu</div>
+                                        <div>
+                                            <svg class=" w-6 h-6 hover:text-gray-500 "
                                                 x-show="!showMenu"
                                                 fill="none"
                                                 stroke-linecap="round"
                                                 stroke-linejoin="round"
                                                 stroke-width="2"
                                                 viewBox="0 0 24 24"
-                                                stroke="currentColor"
-                                            >
+                                                stroke="currentColor">
                                                 <path d="M4 6h16M4 12h16M4 18h16"></path>
                                             </svg>
-                                        </button>
-                                    </div> */}
+                                        </div>
+                                    </div>
+                                </button>
+                                <div class="hidden mobile-menu text-white">
+                                    <div class="flex flex-col text-center px-10 hover:text-blue-500 transition duration-300" >
+                                        <div class="border-b pb-2"></div>
+                                        <div class="px-5 py-1 border-b"><a href="/" class={isPathMatching("/") ? 'font-bold' : ''} >முகப்பு/Home</a></div>
+                                        <div class="px-5 py-1 border-b"><a href="/videos" class={isPathMatching("/videos") ? 'font-bold' : ''}>காணொளிகள்/Videos</a></div>
+                                        <div class="px-5 py-1 border-b"><a href="/works" class={isPathMatching("/works") ? 'font-bold' : ''}>திருப்பணி/Renovation </a></div>
+                                        <div class="px-5 pt-1"><a href="/contact" class={isPathMatching("/contact") ? 'font-bold' : ''}>தொடர்பு & வழி/Contact</a></div>
+                                    </div>
                                 </div>
                             </div>
-                            {/* <div class="hidden mobile-menu">
-                                <ul class="">
-                                    <li class="active"><a href="/" class="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
-                                    <li><a href="/videos" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Services</a></li>
-                                </ul>
-                            </div> */}
                         </nav>
                     </div>
                 </div>
